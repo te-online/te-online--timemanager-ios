eccbd75ab6c52b3111ec0d584967f2d4da548ef0
Thomas Ebert <thomas.ebert@te-online.net>
2016-08-09 10:26:13 +0200

    Try to use a page view controller fro side-by-side view.

--- TimeManager/TimeManager/ClientTableViewController.swift
+++ TimeManager/TimeManager/ClientTableViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ClientTableViewController: UITableViewController {
     
+    var clients = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        clients = ["CHOAM", "Acme Corp.", "Sirius Cybernetics Corp.", "Rich Industries", "Evil Corp.", "Soylent Corp.", "Very Big Corp. of America", "Frobozz Magic Co.", "Warbucks Industries", "Tyrell Corp.", "Wayne Enterprises", "Virtucon", "Globex", "Umbrella Corp.", "Wonka Industries", "Stark Industries", "Clampett Oil", "Oceanic Airlines", "Yoyodyne Propulsion Sys.", "Cyberdyne Systems Corp.", "d’Anconia Copper", "Gringotts", "Oscorp", "Nakatomi Trading", "Spacely Space Sprockets"]
     }
     
     override func didReceiveMemoryWarning() {
@@ -22,13 +26,13 @@ class ClientTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("clients") as UITableViewCell!
-        cell.textLabel?.text = "Hello"
+        cell.textLabel?.text = clients[indexPath.row]
         
         return cell
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return clients.count
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
--- TimeManager/TimeManager/ViewController.swift
+++ TimeManager/TimeManager/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UINavigationController, UIPageViewControllerDataSource {
+
+    var pageViewController: UIPageViewController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("pageViewController") as? UIPageViewController
+        self.pageViewController.dataSource = self
+        
+        let clientsController = storyboard?.instantiateViewControllerWithIdentifier("ClientsViewController")
+        self.pageViewController.setViewControllers([clientsController!], direction: .Forward, animated: true, completion: nil)
+        
+        self.addChildViewController(self.pageViewController)
+        self.view.addSubview(self.pageViewController.view)
+        self.pageViewController.didMoveToParentViewController(self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
 
-
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 2
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
 }
 
